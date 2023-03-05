72c033c4282f7a0bba50f0efa31e920858b72fc3
RicardoJun10r <ricardojunior0110@gmail.com>
2023-03-05 18:30:23 -0300

    Enviando resolucao do problema climbing stairs

---
+++ Challenges/src/ClimbingStairs/src/Solution.java
@@ -0,0 +1,25 @@
+public class Solution
+{
+	public static void main(String[]args)
+	{
+		System.out.println(climbStairs(3));
+	}
+
+	public static int climbStairs(int n){
+
+		int resultado = 0, proximo = 1, anterior = 1;
+		if(n > 1)
+		{
+			for(int i = 0; i < n; i++){
+				resultado = anterior + proximo;
+				anterior = proximo;
+				proximo = resultado;
+			}
+		} else {
+
+			return 1;
+		
+		}
+		
+	}
+}
